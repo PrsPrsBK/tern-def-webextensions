a0eb94a6ce64f200c1a74917ceee91aeca1268eb
PrsPrsBK <prsprsbk@gmail.com>
2018-04-08 22:47:18 +0900

    chore: note

--- build.js
+++ build.js
@@ -1,25 +1,30 @@
+/**
+ * @fileoverview anyway make tern-definition files, or survey about mozilla source tree.
+ * @author PrsPrsBK
+ */
+
 const readline = require('readline');
 const fs = require('fs');
 const path = require('path');
 const stripJsonComments = require('strip-json-comments');
-const lisenceRegex = /licen[cs]{1}e/i;
 const targetJsons = [
   {
-    toFile: 'webextensions-general.json',
+    toFile: 'webextensions-general',
     fromDir: 'toolkit/components/extensions/schemas/',
   },
   {
-    toFile: 'webextensions-firefox-desktop.json',
+    toFile: 'webextensions-firefox-desktop',
     fromDir: 'browser/components/extensions/schemas/',
   },
   {
-    toFile: 'webextensions-firefox-android.json',
+    toFile: 'webextensions-firefox-android',
     fromDir: 'mobile/android/components/extensions/schemas/',
   },
 ];
 
 let repositoryDir = '';
 let isSurvey = false;
+let releaseChannel = 'beta';
 
 const processArgs = () => {
   process.argv.forEach((arg, idx, a) => {
@@ -29,9 +34,12 @@ const processArgs = () => {
     else if(arg === '--survey') {
       isSurvey = true;
     }
+    else if(arg === '--channel') {
+      releaseChannel = process.argv[idx + 1];
+    }
   });
   if(repositoryDir === '') {
-    console.log('please specify: npm run build -- --repo actual-your-repository-path');
+    console.log('please specify: npm run build -- --repo actual-your-mozilla-foo-repository-path');
     return false;
   }
   else {
@@ -103,15 +111,50 @@ const survey = () => {
   });
 };
 
+/*
+ * create 3 definition-files.
+ *   '3' means that
+ *     - genaral API
+ *     - browser UI API
+ *     - android UI API
+ *   each API has own manifest.
+ *     - toolkit/components/extensions/extensions-toolkit.manifest
+ *       - ext-toolkit.json
+ *       - ignore
+ *         - ext-toolkit.js
+ *         - ext-tabs-base.js
+ *         - ext-c-toolkit.js
+ *         - events.json
+ *         - native_manifest.json
+ *         - types.json
+ *     - browser/components/extensions/extensions-browser.manifest
+ *       - ext-browser.json
+ *       - ignore
+ *         - ext-browser.js has no registerModules()
+ *         - ext-c-browser.js has registerModules()
+ *           - but ignore. covered by ext-browser
+ *         - menus_internal.json
+ *     - mobile/android/components/extensions/extensions-mobile.manifest
+ *       - no json
+ *       - ext-android.js has registerModules()
+ *         - browserAction
+ *         - pageAction
+ *         - tabs
+ *       - ext-utils.js has no registerModules()
+ *       - ext-c-android.js has registerModules()
+ *         - tabs
+ */
 const build = () => {
   let result = { "!name": "webextensions" };
   targetJsons.forEach((targetUnit) => {
     const files = fs.readdirSync(path.join(repositoryDir, targetUnit.fromDir));
-    console.log(files.length);
     files.filter(name => name.endsWith('.json')).forEach((jsonName, i, a) => {
       const jsonNameFull = path.join(repositoryDir, targetUnit.fromDir, jsonName);
       const orig = JSON.parse(stripJsonComments(fs.readFileSync(jsonNameFull, 'utf8')));
-      console.log(`${jsonNameFull}: ${orig[0].namespace}`);
+      console.log(`${jsonName}: ${orig[0].namespace}: ${orig.length}`);
+      orig.forEach((sth) => {
+        console.log(`  namespace: ${sth.namespace}`);
+      });
       let converted = {};
       //abstract and convert
     });
