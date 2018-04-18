9e1db91446c583dbd041756b2a3edf793a0d8b43
PrsPrsBK <prsprsbk@gmail.com>
2018-04-19 01:56:55 +0900

    feat: postpublish script. shift from js to pwsh, and can renew which_is_used.txt

---
+++ regist_pub_status.ps1
@@ -0,0 +1,42 @@
+<#
+ # @fileoverview run as postpublish.
+ # 1. update defs/which_is_used.txt
+ # 2. delete cset_pub.log and rename cset.log to cset_pub.log
+ # @author PrsPrsBK
+#>
+
+Param(
+  [Parameter(Position = 0, Mandatory = $true)]
+  [ValidateScript({
+    if((Resolve-Path $_).Provider.Name -ne "FileSystem") {
+       throw "Please specify mozilla-xxx repository path : '$_'"
+    }
+    return $true
+  })]
+  [string]$mozilla_repo
+)
+
+Begin {
+  Get-Command hg -ErrorAction:Ignore | Out-Null
+  if($? -eq $false) {
+    throw "hg does not exist on the PATH."
+  }
+}
+
+Process {
+  $script_path =  (Split-Path -Parent $MyInvocation.MyCommand.Path)
+  $which_is_used =  (Join-Path -Path $script_path -ChildPath "defs/which_is_used.txt")
+
+  Start-Job -ArgumentList $mozilla_repo, $which_is_used -ScriptBlock {
+    Param($repo, $log)
+    hg log -l 1 -R $repo --template "mozilla-beta changeset: {rev}:{node}" > $log
+  } | Wait-Job | Receive-Job | Remove-Job
+
+  #TODO: delete cset_pub.log and rename cset.log to cset_pub.log
+}
+
+End {
+}
+
+# vim:expandtab ff=dos fenc=utf-8 sw=2
+
