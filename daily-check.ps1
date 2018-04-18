21ff11e9ba015442be9118e111d766b5fee9663b
PrsPrsBK <prsprsbk@gmail.com>
2018-04-18 19:34:23 +0900

    feat: daily-check can judge we need update or not

--- daily-check.ps1
+++ daily-check.ps1
@@ -11,30 +11,53 @@ Param(
 )
 
 Begin {
+  Get-Command hg -ErrorAction:Ignore | Out-Null
+  if($? -eq $false) {
+    throw "hg does not exist on the PATH."
+  }
   $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "incoming -R $mozilla_repo --quiet" -NoNewWindow -PassThru)
   Wait-Process -Id $hg_proc.id
-  if($LASTEXITCODE -eq 1) {
-    Write-Host "Nothing to pull."
-  }
-  else {
-    $yn = Read-Host "There are incoming. hg pull -u? [y/n]"
+  if($LASTEXITCODE -eq 0) {
+    $yn = Read-Host "incomings may exist. hg pull -u? [y/n]"
     if($yn -eq "y") {
-      $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "pull -u -R $mozilla_repo" -NoNewWindow -PassThru)
-      Wait-Process -Id $hg_proc.id
+        $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "pull -u -R $mozilla_repo" -NoNewWindow -PassThru)
+        Wait-Process -Id $hg_proc.id
     }
   }
+  else {
+    Write-Host "nothing to pull."
+  }
+  $hg_proc = $null
 }
 
 Process {
-  Push-Location (Split-Path -Parent $MyInvocation.MyCommand.Path)
+  $script_path =  (Split-Path -Parent $MyInvocation.MyCommand.Path)
+  $cset_pubed =  (Join-Path -Path $script_path -ChildPath "cset_pubed.log")
+  $cset_today =  (Join-Path -Path $script_path -ChildPath "cset.log")
 
-  $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "log -l 3 -R $mozilla_repo -I", (Join-Path -Path $mozilla_repo -ChildPath "/toolkit/components/extensions/schemas/*.json"), "--removed --template status" -NoNewWindow -PassThru)
-  Wait-Process -Id $hg_proc.id
+  Start-Job -ArgumentList $mozilla_repo, $cset_today -ScriptBlock {
+    Param($repo, $log)
+    hg log -l 3 -R $repo -I (Join-Path -Path $repo -ChildPath "/toolkit/components/extensions/schemas/*.json") --removed --template status > $log
+  } | Wait-Job | Receive-Job | Remove-Job
 
-  $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "log -l 3 -R $mozilla_repo -I", (Join-Path -Path $mozilla_repo -ChildPath "/browser/components/extensions/schemas/*.json"), "--removed --template status" -NoNewWindow -PassThru)
-  Wait-Process -Id $hg_proc.id
+  Start-Job -ArgumentList $mozilla_repo, $cset_today -ScriptBlock {
+    Param($repo, $log)
+    # append !!!!!!!!!
+    hg log -l 3 -R $repo -I (Join-Path -Path $repo -ChildPath "/browser/components/extensions/schemas/*.json") --removed --template status >> $log
+  } | Wait-Job | Receive-Job | Remove-Job
 
-  Pop-Location
+  if(Test-Path -Path $cset_pubed) {
+    if(Compare-Object (Get-Content $cset_pubed) (Get-Content $cset_today)) {
+      Write-Host "May need to UPDATE!!!!!!!!!!!"
+    }
+    else {
+      Write-Host "no change added."
+    }
+  }
+  else {
+    Write-Host "first time"
+    Copy-Item $cset_today $cset_pubed
+  }
 }
 
 End {
