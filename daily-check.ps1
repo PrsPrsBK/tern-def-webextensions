a0d9c63983b508d7242b5df4241b8ad978de65b8
PrsPrsBK <prsprsbk@gmail.com>
2019-02-11 01:39:00 +0900

    improve daily-check.ps1

--- daily-check.ps1
+++ daily-check.ps1
@@ -2,12 +2,13 @@
 Param(
   [Parameter(Position = 0, Mandatory = $true)]
   [ValidateScript({
-    if(Test-Path $_ -eq $False -Or (Resolve-Path $_).Provider.Name -ne "FileSystem") {
+    if(-Not (Test-Path $_) -Or (Resolve-Path $_).Provider.Name -ne "FileSystem") {
       throw "Please specify valid mozilla-xxx repository path : '$_'"
     }
     return $true
   })]
-  [string]$mozilla_repo
+  [string]$mozilla_repo,
+  [switch]$SkipFetch
 )
 
 Begin {
@@ -15,23 +16,28 @@ Begin {
   if($? -eq $false) {
     throw "hg does not exist on the PATH."
   }
-  $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "incoming -R $mozilla_repo --quiet" -NoNewWindow -Wait -PassThru)
-  Wait-Process -Id $hg_proc.id
-  # System.Diagnostics.Process.ExitCode with -Wait is necessary. $LASTEXITCODE does not work.
-  if($hg_proc.ExitCode -eq 0) {
-    [ValidateSet("y","n")]$yn = Read-Host "incomings may exist. hg pull -u? [y/n]"
-    if($yn -eq "y") {
-      Start-Process -FilePath "hg" -ArgumentList "pull -u -R $mozilla_repo" -NoNewWindow -PassThru | Wait-Process
-    }
-    else {
-      Write-Host "not 'yes', so nothing pulled. "
-    }
+  if($SkipFetch) {
+    Write-Host "Skip 1st phase: no fetch from remote."
   }
   else {
-    Write-Host "nothing to pull. hg ExitCode: $($hg_proc.ExitCode)"
+    $hg_proc = (Start-Process -FilePath "hg" -ArgumentList "incoming -R $mozilla_repo --quiet" -NoNewWindow -Wait -PassThru)
+    Wait-Process -Id $hg_proc.id
+    # System.Diagnostics.Process.ExitCode with -Wait is necessary. $LASTEXITCODE does not work.
+    if($hg_proc.ExitCode -eq 0) {
+      [ValidateSet("y","n")]$yn = Read-Host "incomings may exist. hg pull -u? [y/n]"
+      if($yn -eq "y") {
+        Start-Process -FilePath "hg" -ArgumentList "pull -u -R $mozilla_repo" -NoNewWindow -PassThru | Wait-Process
+      }
+      else {
+        Write-Host "not 'yes', so nothing pulled. "
+      }
+    }
+    else {
+      Write-Host "nothing to pull. hg ExitCode: $($hg_proc.ExitCode)"
+    }
+    $hg_proc = $null
   }
-  $hg_proc = $null
-  Write-Host "NEXT PHASE: check if we need to update or not."
+  Write-Host "Next phase: check if we need to update or not."
 }
 
 Process {
@@ -52,14 +58,14 @@ Process {
 
   if(Test-Path -Path $cset_pubed) {
     if(Compare-Object (Get-Content $cset_pubed) (Get-Content $cset_today)) {
-      Write-Host "May need to UPDATE!!!!!!!!!!!"
+      Write-Host "Result: May need to UPDATE!!!!!!!!!!!"
     }
     else {
-      Write-Host "no change added."
+      Write-Host "Result: no change added."
     }
   }
   else {
-    Write-Host "first time"
+    Write-Host "Result: first time."
     Copy-Item $cset_today $cset_pubed
   }
 }
